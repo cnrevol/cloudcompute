a5e2d4009038df68076e35066997beff2e475d22
Jie Song <cnrevol@hotmail.com>
2026-04-03 14:49:46 +0800

    Update HelloController.java

--- devops/projects/webconfrim/java-spring-demo/src/main/java/com/example/demo/HelloController.java
+++ devops/projects/webconfrim/java-spring-demo/src/main/java/com/example/demo/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello() {
         return "Hello Docker DevOps";
     }
+
+    @GetMapping("/hi")
+    public String hello2() {
+        return "Hi Bro, come on. ";
+    }
 }
