a15cdac1bf16b9391592bd3cea18e47c182a24fe
Jie Song <cnrevol@hotmail.com>
2026-04-03 14:36:01 +0800

    Update HelloController.java

--- devops/projects/webconfrim/java-spring-demo/src/main/java/com/example/demo/HelloController.java
+++ devops/projects/webconfrim/java-spring-demo/src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello DevOps";
+        return "Hello Docker DevOps";
     }
 }
